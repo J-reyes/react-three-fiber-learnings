2a3029caf131c58546f825dba9f5cd92f9940b90
Jose Reyes <vazquez.jr09@gmail.com>
2021-07-22 23:34:36 -0700

    added orit controls + axes helper

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import "./App.css";
-import { Canvas, useFrame } from "react-three-fiber";
+import { Canvas, useFrame, extend, useThree } from "react-three-fiber";
 import { useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// extend orbit controls to use it inside of jsx
+extend({ OrbitControls });
 
-const Box = () => {
+const Orbit = () => {
+  // destruct
+  const { camera, gl } = new useThree();
+  return <orbitControls args={[camera, gl.domElement]} />;
+};
+
+const Box = (props) => {
   // reference the box
   const boxRef = useRef();
 
   // used to animate box
   useFrame((state) => {
-    console.log(boxRef)
+    console.log(boxRef);
     // .current is from the useRef api
     boxRef.current.rotation.x += 0.01;
     boxRef.current.rotation.y += 0.01;
   });
 
   return (
-    <mesh ref={boxRef}>
+    <mesh ref={boxRef} {...props}>
       <boxBufferGeometry />
-      <meshBasicMaterial color="blue"/>
+      <meshBasicMaterial color="blue" />
     </mesh>
   );
 };
@@ -25,8 +34,10 @@ const Box = () => {
 function App() {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
-      <Canvas style={{ background: "black" }}>
-        <Box />
+      <Canvas style={{ background: "black" }} camera={{ position: [3, 3, 3] }}>
+        <Box position={[-1, -1, 2]} />
+        <Orbit />
+        <axesHelper args={[5]} />
       </Canvas>
     </div>
   );
