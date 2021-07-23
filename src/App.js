d97af96de5049ca131f4ce8bec8b5675fb7d3e7d
Jose Reyes <vazquez.jr09@gmail.com>
2021-07-22 22:47:30 -0700

    created first scene

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
-import {Canvas} from 'react-scripts'
+import "./App.css";
+import { Canvas, useFrame } from "react-three-fiber";
+import { useRef } from "react";
+
+const Box = () => {
+  // reference the box
+  const boxRef = useRef();
+
+  // used to animate box
+  useFrame((state) => {
+    console.log(boxRef)
+    // .current is from the useRef api
+    boxRef.current.rotation.x += 0.01;
+    boxRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={boxRef}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color="blue"/>
+    </mesh>
+  );
+};
 
 function App() {
   return (
-    <Canvas>
-      
-    </Canvas>
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Canvas style={{ background: "black" }}>
+        <Box />
+      </Canvas>
+    </div>
   );
 }
 
