fc09e6e935d66153313c2ea1c163376d132cf684
Jose Reyes <vazquez.jr09@gmail.com>
2021-07-26 22:44:13 -0700

    Added events to our box meshes + a onClick function thhat changes the color of the box

--- src/App.js
+++ src/App.js
@@ -26,11 +26,40 @@ const Box = (props) => {
   const texture = useLoader(THREE.TextureLoader, "/wood.jpeg");
   // used to animate box
   useFrame((state) => {
-    // console.log(boxRef);
-    // .current is from the useRef api
     boxRef.current.rotation.y += 0.01;
     boxRef.current.rotation.x += 0.01;
   });
+
+  const handlePointDown = (e) => {
+    // save mesh to window
+    // dont do this wit production code
+    e.object.active = true;
+
+    // if there is an active mesh scale it down
+    if (window.activeMesh) {
+      scaleDown(window.activeMesh);
+    }
+    window.activeMesh = e.object;
+  };
+
+  const handlePointerEnter = (e) => {
+    e.object.scale.x = 1.5;
+    e.object.scale.y = 1.5;
+    e.object.scale.z = 1.5;
+  };
+
+  const handlePointerLeave = (e) => {
+    // if its active it wont scale down
+    if (!e.object.active) {
+      scaleDown(e.object);
+    }
+  };
+
+  const scaleDown = (object) => {
+    object.scale.x = 1;
+    object.scale.y = 1;
+    object.scale.z = 1;
+  };
 
   return (
     <mesh
@@ -38,6 +67,9 @@ const Box = (props) => {
       {...props}
       castShadow
       // receiveShadow
+      onPointerDown={handlePointDown}
+      onPointerEnter={handlePointerEnter}
+      onPointerLeave={handlePointerLeave}
     >
       <boxBufferGeometry />
       <meshPhysicalMaterial map={texture} />
@@ -46,13 +78,12 @@ const Box = (props) => {
 };
 
 const Background = (props) => {
-  const { scene } = useThree()
-  const texture = useLoader(THREE.TextureLoader, 'autoshop.jpeg')
-  texture.mapping = THREE.EquirectangularReflectionMapping
-  scene.background = texture
-  return null
+  const { scene } = useThree();
+  const texture = useLoader(THREE.TextureLoader, "autoshop.jpeg");
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+  return null;
 };
-
 
 const Floor = (props) => {
   return (
@@ -74,18 +105,41 @@ const Bulb = (props) => {
 };
 
 function App() {
+  const handleClick = e => {
+    // if no active mesh on window return otherwise change color of active mesh
+    if (!window.activeMesh) return;
+    window.activeMesh.material.color = new THREE.Color(e.target.style.background)
+  }
+
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
+      <div style={{ position: "absolute", zIndex: 1 }}>
+        <div
+          onClick={handleClick}
+          style={{ background: "blue", height: 50, width: 50 }}
+        ></div>
+        <div
+          onClick={handleClick}
+          style={{ background: "yellow", height: 50, width: 50 }}
+        ></div>
+        <div
+          onClick={handleClick}
+          style={{ background: "white", height: 50, width: 50 }}
+        ></div>
+      </div>
       <Canvas
         shadows
         style={{ background: "black" }}
-        camera={{ position: [3, 3, 3] }}
+        camera={{ position: [7, 7, 7] }}
       >
         {/* <fog attach="fog" args={["white", 1, 10]} /> */}
         <ambientLight intensity={0.2} />
         <Bulb position={[0, 3, 0]} />
         <Suspense fallback={null}>
-          <Box position={[0, 1, 0]} />
+          <Box position={[-4, 1, 0]} />
+        </Suspense>
+        <Suspense fallback={null}>
+          <Box position={[4, 1, 0]} />
         </Suspense>
         <Suspense fallback={null}>
           <Background />
